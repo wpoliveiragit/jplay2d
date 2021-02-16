bb448515a07b85ef388554c7d46961aa4ade6faa
wpoliveiragit <wp_oliveira@yahoo.com.br>
2021-02-16 01:57:54 -0300

    adicionado a base do projeto

---
+++ src/test/java/br/com/wellington/jplay2D/Jplay2DApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wellington.jplay2D;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jplay2DApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
